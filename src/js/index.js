ec41a023a42f81884e518e5d1ff4250d26023fb5
Freasy-Vlad <vladdanilchenkodune@gmail.com>
2022-11-06 23:14:26 +0200

    add markup and pagination

---
+++ src/js/index.js
@@ -0,0 +1,72 @@
+import 'material-icons/iconfont/material-icons.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { UnsplashAPI } from './UnsplashAPI';
+import { createMarkup } from './createMarkup';
+import { refs } from './refs';
+
+const unsplash = new UnsplashAPI();
+// console.log(unsplash);
+
+const handleSubmit = event => {
+  event.preventDefault();
+
+  const {
+    elements: { query },
+  } = event.currentTarget;
+  const searchQuery = query.value.trim().toLowerCase();
+  if (!searchQuery) {
+    Notify.failure('Введіть дані для пошуку!!!');
+    return;
+  }
+  unsplash.query = searchQuery;
+  clearPage();
+
+  unsplash
+    .getPhotos()
+    .then(({ results, total }) => {
+      if (results.length === 0) {
+        Notify.info(`За вашим запитом ${searchQuery} зображень не знайдено!`);
+        return;
+      }
+      const markup = createMarkup(results);
+      refs.list.insertAdjacentHTML('beforeend', markup);
+
+      unsplash.calculateTotalPages(total);
+
+      if (unsplash.isShowLoadMore) {
+        refs.loadMoreBtn.classList.remove('is-hidden');
+      }
+    })
+    .catch(error => {
+      Notify.failure(error.message, 'Щось пішо не так!');
+      clearPage();
+    });
+};
+
+const onLoadMore = () => {
+  unsplash.incrementPage();
+
+  if (!unsplash.isShowLoadMore) {
+    refs.loadMoreBtn.classList.add('is-hidden');
+  }
+
+  unsplash
+    .getPhotos()
+    .then(({ results }) => {
+      const markup = createMarkup(results);
+      refs.list.insertAdjacentHTML('beforeend', markup);
+    })
+    .catch(error => {
+      Notify.failure(error.message, 'Щось пішо не так!');
+      clearPage();
+    });
+};
+
+refs.form.addEventListener('submit', handleSubmit);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+
+function clearPage() {
+  unsplash.resetPage();
+  refs.list.innerHTML = '';
+  refs.loadMoreBtn.classList.add('is-hidden');
+}
